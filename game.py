86c071e15a3a657b8b38928add51d8872633b173
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-18 11:42:28 +0600

    add ball and  bar images, creat move function

--- game.py
+++ game.py
@@ -5,11 +5,46 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = 'PONG GAME'
 
 
+
+
+class Bar(arcade.Sprite):
+    def __init__(self):
+        super().__init__('bar.jpg', 0.5)
+
+
+class Ball(arcade.Sprite):
+    def __init__(self):
+        super().__init__('ball.jpg', 0.3)
+        self.change_x = 2
+        self.change_y = 2
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+
+
 class Game(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.bar = Bar()
+        self.ball = Ball()
+        self.setup()
+
+    def setup(self):
+        self.bar.center_x = SCREEN_WIDTH / 2
+        self.bar.center_y = SCREEN_HEIGHT / 7
+        self.ball.center_x = SCREEN_WIDTH / 2
+        self.ball.center_y = SCREEN_HEIGHT / 2
+
+    
     def on_draw(self):
         self.clear((255, 255, 255))
+        self.bar.draw()
+        self.ball.draw()
 
-
+    def update(self, delta_time: float):
+        self.ball.update()
 
 
 if __name__ == '__main__':
