b53ce463f55a4fa3918110b8b666ea9ab1376fba
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-18 21:53:22 +0600

    creat bar move

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import arcade
+import arcade.key
+import arcade.key
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = 'PONG GAME'
 
@@ -11,16 +13,27 @@ class Bar(arcade.Sprite):
     def __init__(self):
         super().__init__('bar.jpg', 0.5)
 
+    def update(self):
+        self.center_x += self.change_x
+
 
 class Ball(arcade.Sprite):
     def __init__(self):
         super().__init__('ball.jpg', 0.3)
-        self.change_x = 2
-        self.change_y = 2
+        self.change_x = 3
+        self.change_y = 3
 
     def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+        if self.right >= SCREEN_WIDTH:
+            self.change_x = -self.change_x
+        if self.left <= 0:
+            self.change_x = -self.change_x
+        if self.top >= SCREEN_WIDTH:
+            self.change_y = -self.change_y
+        if self.bottom <= 0:
+            self.change_y = -self.change_y
 
 
 
@@ -45,7 +58,15 @@ class Game(arcade.Window):
 
     def update(self, delta_time: float):
         self.ball.update()
+        self.bar.update()
 
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.RIGHT:
+            self.bar.change_x = 2
+        if symbol == arcade.key.LEFT:
+            self.bar.change_x = -2
+    
+    def
 
 if __name__ == '__main__':
     window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
